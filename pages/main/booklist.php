47c1ac5350d73608e6b73b74430b14ea427543eb
yayapipi <yayapipi@hotmail.com>
2019-03-27 15:58:15 +0800

    Add A New Pop-up Design
    pop-up.css
    still need to refign

--- pages/main/booklist.php
+++ pages/main/booklist.php
@@ -9,6 +9,7 @@
   <!-- plugins:css -->
   <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="../../vendors/base/vendor.bundle.base.css">
+  <link rel="stylesheet" href="../../css/popup.css"
   <!-- endinject -->
   <!-- inject:css -->
   <link rel="stylesheet" href="../../css/style.css">
@@ -237,6 +238,10 @@
                 </div>
               </div>
             </div>
+
+
+
+
            
         <!-- content-wrapper ends -->
         <!-- partial:../../partials/_footer.html -->
@@ -254,6 +259,91 @@
     </div>
     <!-- page-body-wrapper ends -->
   </div>
+
+
+
+          <!-- Book-Model-Box -->
+
+
+        <button id="myBtn">Open Modal</button>
+        <!-- The Modal -->
+        <div id="myModal" class="book-modal">
+
+          <!-- Modal content -->
+          <div class="book-modal-content">
+            <div class="modal-body">
+                  <span class="close">&times;</span>
+           <input type="text" >
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in1 the Modal Body</p>
+              <p>Some other text...</p>
+
+              <textarea></textarea>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in1 the Modal Body</p>
+              <p>Some other text...</p>
+
+              <textarea></textarea>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in1 the Modal Body</p>
+              <p>Some other text...</p>
+
+              <textarea></textarea>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in the Modal Body</p>
+              <p>Some other text...</p>
+              <p>Some text in1 the Modal Body</p>
+              <p>Some other text...</p>
+
+              <textarea></textarea>
+            </div>
+
+          </div>
+
+        </div>
+
+        <script>
+
+        // Get the modal
+        var modal = document.getElementById('myModal');
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+        </script>
+
+
+
+
   <!-- container-scroller -->
   <!-- plugins:js -->
   <script src="../../vendors/base/vendor.bundle.base.js"></script>
