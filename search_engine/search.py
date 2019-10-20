ba2fb4c9370cd220e1fe4881023beb4edf19a3c1
yayapipi <yayapipi@hotmail.com>
2019-10-20 15:38:28 +0800

    Search Ascii Remove

--- search_engine/search.py
+++ search_engine/search.py
@@ -6,17 +6,13 @@ from bs4 import BeautifulSoup
 r = requests.get('https://search.books.com.tw/search/query/key/'+sys.argv[1])
 f = codecs.open('search-result.txt','w+',"utf-8")
 if r.status_code == requests.codes.ok:
-  # 以 BeautifulSoup 解析 HTML 程式碼
+
   soup = BeautifulSoup(r.text, 'html.parser')
 
-  #print(soup.prettify())
-  # 以 CSS 的 class 抓出各類頭條新聞
   stories = soup.find_all('a',rel='mid_image')
   for s in stories:
-    # 新聞標題
     print("標題：" + s.get('title'))
     f.write("#Title:" + s.get('title')+"#End")
-    # 新聞網址
     href_cut_start = 'item/'
     href_cut_end = '/page/'
     href = s.get('href')
@@ -27,7 +23,6 @@ if r.status_code == requests.codes.ok:
 
   image = soup.find_all('img',class_='itemcov')
   for i in image:
-    #Get Image URL
     image_cut_start = 'getImage?i='
     image_cut_end = '&'
     image_start_pos = i.get('data-original').index(image_cut_start)
